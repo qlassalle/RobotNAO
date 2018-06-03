878b73935de03479ffc373c125ed6d27aebcb330
Quentin Lassalle <qlassalle@gmail.com>
2018-06-03 17:19:02 +0200

    debut d ajout du code pour donner les projets du jour

--- src/readcsv.py
+++ src/readcsv.py
@@ -29,12 +29,30 @@ description = []
 date = []
 for row in data[1:]:
     ligne = row[0].split(';')
-    intervenants.append(ligne[0])
+    intervenants.append(str(ligne[0]))
     projet.append(ligne[1])
     description.append(ligne[2])
     date.append(ligne[3])
 
-print intervenants
-print projet
-print description
+print date
+#!/usr/bin/python
+ 
+import time
+## dd/mm/yyyy format
+listeMois = ["janvier", "fevrier", "mars", "avril", "mai", "juin", "juillet", "aout", "septembre", "octobre", "novembre", "decembre"]
+currDate = time.strftime("%d/%m/%Y")
+print currDate
+mois = currDate[3] + currDate[4]
+jourTmp = currDate[0] + currDate[1]
+jour = jourTmp.lstrip("0")
+newDate = jour + " " + listeMois[int(mois) - 1] + " " + currDate[6] + currDate[7] + currDate[8] + currDate[9]
+
+for aDate in date:
+    if aDate == newDate:
+        print "equals ! " + newDate
+
+
+
+from datetime import datetime
+date = datetime.now()
 print date
