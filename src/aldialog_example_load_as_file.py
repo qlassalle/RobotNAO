88329eb39aba9e3b76926520ecea7bf5c1145a70
marcSLSD <marc.salsedo78@gmail.com>
2018-06-07 10:23:47 +0200

    modification du fichier py de lancement AJOUT Interface graphique

--- src/aldialog_example_load_as_file.py
+++ src/aldialog_example_load_as_file.py
@@ -1,3 +1,4 @@
+
 #! /usr/bin/env python
 # -*- encoding: UTF-8 -*-
 
@@ -6,21 +7,74 @@
 import qi
 import argparse
 import sys
-import subprocess
 import os
-from datetime import datetime
 
-def main(session, topic_path):
+from Tkinter import *
+from ScrolledText import *
+
+
+
+root = Tk()
+root.title("Robot NAO")
+content = Frame(root)
+var = StringVar()
+
+frame = ScrolledText(content, borderwidth=5, relief="sunken", width=30, height=10)
+adresseIPLabel = Label(content, text="Adresse IP NAO")
+IPtexteEntree = Entry(content, textvariable=var)
+
+session = qi.Session()
+ALDialog = None
+topic_name = None
+
+def quitter():
+    try:
+        global ALDialog
+        global session
+        global topic_name
+        global root
+        ALDialog.unsubscribe('my_dialog_example')
+        # Deactivating the topic
+        # Loading the topic given by the user (absolute path is required)
+        ALDialog.deactivateTopic(topic_name)
+        # now that the dialog engine is stopped and there are no more activated topics,
+        # we can unload our topic and free the associated memory
+        ALDialog.unloadTopic(topic_name)
+        session.close()
+        root.destroy()
+        sys.exit()
+    except RuntimeError:
+        print ("\nCan't connect to Naoqi at IP {} (port {}).\nPlease check your script's arguments. Run with -h option for help.\n".format("192.168.43.232", 9559))
+        root.destroy()
+        sys.exit(1)
+
+
+def report_callback_exception(self, exc, val, tb):
+        showerror("Error", message=str(val))
+        frame.insert(END,"\n"+"Impossible de se connecter avec cette IP \nEssayez d'appuyer sur le torse de nao pour obtenir son adresse IP")
+
+def main():
+    ip = var.get()
+    frame.insert(END,"\n"+"Demarrage sur ip : "+ip)
+    try:
+        global session
+        global ALDialog
+        session.connect("tcp://{}:{}".format(str(ip), 9559))
+        ALDialog = session.service("ALDialog")
+    except RuntimeError:
+        raise Exception("Impossible de se connecter")
+        
     """
     This example uses ALDialog methods.
     It's a short dialog session with one topic.
     """
     # Getting the service ALDialog
-    ALDialog = session.service("ALDialog")
-    ALDialog.setLanguage("French")
+    frame.insert(END,"\n"+"Chargement du dialogue")
 
+    topic_path="/home/nao/dialog/interactive_dialog.top"
     # Loading the topic given by the user (absolute path is required)
     topf_path = topic_path.decode('utf-8')
+    global topic_name
     topic_name = ALDialog.loadTopic(topf_path.encode('utf-8'))
 
     # Activating the loaded topic
@@ -30,35 +84,21 @@ def main(session, topic_path):
     # We subscribe only ONCE, regardless of the number of topics we have activated
     ALDialog.subscribe('my_dialog_example')
 
-    try:
-        raw_input("\nSpeak to the robot using rules from the just loaded .top file. Press Enter when finished:")
-    finally:
-        # stopping the dialog engine
-        ALDialog.unsubscribe('my_dialog_example')
+content.grid(column=0, row=0, sticky=(N, S, E, W))
+frame.grid(column=0, row=0, columnspan=1, rowspan=2, sticky=(N, S, E, W))
+adresseIPLabel.grid(column=3, row=0, columnspan=2, sticky=(N, W), padx=5)
+IPtexteEntree.grid(column=3, row=1, columnspan=2, sticky=(N, E, W), pady=5, padx=5)
+boutonDemarrer = Button(content, text="Demarrer", command = lambda: main())
+boutonArreter = Button(content, text="Arreter", command = lambda: quitter())
+boutonDemarrer.grid(column=3, row=3)
+boutonArreter.grid(column=4, row=3)
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+content.columnconfigure(0, weight=3)
+content.columnconfigure(1, weight=3)
+content.columnconfigure(2, weight=3)
+content.columnconfigure(3, weight=1)
+content.columnconfigure(4, weight=1)
+content.rowconfigure(1, weight=1)
 
-        # Deactivating the topic
-        ALDialog.deactivateTopic(topic_name)
-
-        # now that the dialog engine is stopped and there are no more activated topics,
-        # we can unload our topic and free the associated memory
-        ALDialog.unloadTopic(topic_name)
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--ip", type=str, default="192.168.43.232",
-                        help="Robot's IP address. If on a robot or a local Naoqi - use '127.0.0.1' (this is the default value).")
-    parser.add_argument("--port", type=int, default=9559,
-                        help="port number, the default value is OK in most cases")
-    parser.add_argument("--topic-path", type=str, required=True,
-                        help="absolute path of the dialog topic file (on the robot)")
-
-    args = parser.parse_args()
-    session = qi.Session()
-    try:
-        session.connect("tcp://{}:{}".format(args.ip, args.port))
-    except RuntimeError:
-        print ("\nCan't connect to Naoqi at IP {} (port {}).\nPlease check your script's arguments."
-               " Run with -h option for help.\n".format(args.ip, args.port))
-        sys.exit(1)
-    main(session, args.topic_path)
+root.mainloop()
